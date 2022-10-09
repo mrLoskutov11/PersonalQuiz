99864fa27ba9c555d753621f9de6dc615f88aa81
Anton Loskutov <a424056193@me.com>
2022-10-09 14:38:48 +0300

    Initial Commit

---
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ResultViewController.swift
+//  PersonalQuiz
+//
+//  Created by Alexey Efimov on 06.10.2022.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    
+    @IBOutlet var resultsOutlet: UIStackView!
+    
+    
+    // 1. Избавиться от кнопки возврата назад на экране результатов
+    // 2. Передать массив с ответами на экран с результатами
+    // 3. Определить наиболее часто встречающийся тип животного
+    // 4. Отобразить результаты в соответствии с этим животным
+
+    override func viewDidLoad() {
+        navigationItem.hidesBackButton = true
+        super.viewDidLoad()
+    }
+    
+    @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
+        navigationController?.dismiss(animated: true)
+        
+    }
+    
+    deinit {
+        print("\(type(of: self)) has been deallocated")
+    }
+}
+    
+
